8e689648c96c6787c4e34520649bb0f2c004e111
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2024-11-07 00:24:42 +0300

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'), flowbite.plugin(),],
+  plugins: [require('daisyui'),],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "none", // name of one of the included themes for dark mode
