0e5bfd476fb9ceb56ffd2d97e05cdfdcfbfac714
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-02-11 15:20:19 +0400

    phi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,26 +20,14 @@ function Home() {
                             className="mx-auto h-32"
                         />
                         <figure className="mt-10">
-                            <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
+                            <blockquote className="text-center  text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
                                 <p>
-                                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                                    “Təhsil Günəşdir!<br />
+                                    O sizin gələcək həyatınızı daha da işıqlı edəcək!<br />
+                                    Günəşli sabahlara bizimlə addımla.”
                                 </p>
                             </blockquote>
-                            <figcaption className="mt-10">
-                                <img
-                                    alt=""
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    className="mx-auto h-10 w-10 rounded-full"
-                                />
-                                <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                                    <div className="font-semibold text-gray-900">PhD. Lalə Nəsibova</div>
-                                    <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
-                                        <circle r={1} cx={1} cy={1} />
-                                    </svg>
-                                    <div className="text-gray-600">Direktor </div>
-                                </div>
-                            </figcaption>
+
                         </figure>
                     </div>
                 </section>
