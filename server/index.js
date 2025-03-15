bc8050f15f3f0fe70ad400318eeb59a75caf2a2c
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-03-15 16:00:51 +0300

    hun

--- server/index.js
+++ server/index.js
@@ -3,7 +3,17 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',  // Set only one origin
+    credentials: true,  // Required for cookies/sessions
+}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 const { sendContactForm } = require('../controllers/authController');
 
-router.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+
 
 router.post('/kontakt', sendContactForm);
 
