3e254b7a09cb93e158a6a13816e261147ac384ff
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-03-15 16:14:44 +0300

    bran

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors({
     credentials: true,  // Required for cookies/sessions
 }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://ingla.vercel.app");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
