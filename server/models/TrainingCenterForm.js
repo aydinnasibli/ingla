cc3c11cd6c1081b461d0ad2cc260e2dd83864445
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-03-22 12:33:32 +0300

    g

--- server/models/TrainingCenterForm.js
+++ server/models/TrainingCenterForm.js
@@ -20,7 +20,7 @@ const TrainingCenterSchema = new Schema({
     },
     course: {
         type: String,
-        enum: ["Peşəkar Trener Akademiyası", "Daxili Motivasiya və Performansın Artırılması", "Region Gənclərinin Yay Düşərgəsi", "İnkişaf Akademiyası", "Təcrübəli mentorlarla fərdi görüşlər", "Masterklasslar", "Təcrübə imkanları", "Xidmət Mədəniyyəti və Müştəri Məmnuniyyəti", "Gənclərin İnkişaf Marafonu", "Komanda İdarəetməsi və Effektiv Kommunikasiya", "Karyera Məsləhətçiləri Kursu", "HR Peşəkarı", "Satış Uğurunun Sirləri", "Zamanı İdarə Etmə", "Bədən Dili və Ünsiyyət Bacarıqları", "Mindfulness: Zehni Rahatlıq və Fokuslanma", "PMP: Layihə İdarəetməsində Peşəkarlıq", "Şəxsi Brendinq və Sosial Media Strategiyaları", "Mobiliqrafiya və Vizual Kontent Yaratma"],
+        enum: ["Peşəkar Trener Akademiyası", "Karyera məsləhətçilərinin inkişaf kursu", "Rəqəmsal məhsul sahibliyi kursu", "İnsan resurslarının idarə olunması kursu", "Dövlət satınalmaları kursu", "Psixoloji Konsultasiya prosesi kursu", "Yeniyetmə və böyüklərlə psixoterapiya texnikaları kursu", "Daxili Motivasiya və Performansın Artırılması", "Region Gənclərinin Yay Düşərgəsi", "İnkişaf Akademiyası", "Təcrübəli mentorlarla fərdi görüşlər", "Masterklasslar", "Təcrübə imkanları", "Xidmət Mədəniyyəti və Müştəri Məmnuniyyəti", "Gənclərin İnkişaf Marafonu", "Komanda İdarəetməsi və Effektiv Kommunikasiya", "Karyera Məsləhətçiləri Kursu", "HR Peşəkarı", "Satış Uğurunun Sirləri", "Zamanı İdarə Etmə", "Bədən Dili və Ünsiyyət Bacarıqları", "Mindfulness: Zehni Rahatlıq və Fokuslanma", "PMP: Layihə İdarəetməsində Peşəkarlıq", "Şəxsi Brendinq və Sosial Media Strategiyaları", "Mobiliqrafiya və Vizual Kontent Yaratma"],
         required: true
     }
 });
